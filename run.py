f0d935291715742b2e9f71c1b5e1ecbf23d02eff
Jacek Kowalski <Jacek@jacekk.info>
2020-05-18 20:41:52 +0200

    Replace uwsgi with waitress due to 400 Bad Request issues

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import logging
+import os
 
 import docker_plugin_api.Plugin
 import flask
+import waitress
 
 app = flask.Flask('pyIPAM')
 app.logger.setLevel(logging.DEBUG)
@@ -15,4 +17,7 @@ docker_plugin_api.Plugin.functions.append('IpamDriver')
 app.register_blueprint(lib.IpamDriver.app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	if os.environ.get('ENVIRONMENT', 'dev') == 'dev':
+		app.run(debug=True)
+	else:
+		waitress.serve(app, unix_socket='/run/docker/plugins/pyipam.sock', threads=1)
